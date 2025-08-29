00d09785e1a1f07b135e18fc2173719ee9cb56e8
javier-asesores <javier.campos@camposasesores.es>
2025-08-29 13:06:45 +0200

    nuevo plugin

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,17 @@
 return {
-  "folke/snacks.nvim", opts = { indent = { indent = { enabled = false, }, chunk
-    = { enabled = true, char = { horizontal = '─', vertical = '│', corner_top =
-      '╭', corner_bottom = '╰', arrow = '─', }, }, }, picker = { sources = {
-      explorer = { layout = { layout = { width = 32, }, }, }, }, }, }, }
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = { enabled = true },
+    indent = {
+      indent = { enabled = false },
+      chunk = {
+        enabled = true,
+        char = { horizontal = "─", vertical = "│", corner_top = "╭", corner_bottom = "╰", arrow = "─" },
+      },
+    },
+    picker = { sources = {
+      explorer = { layout = { layout = { width = 32 } } },
+    }
+ },
+  },
+}
